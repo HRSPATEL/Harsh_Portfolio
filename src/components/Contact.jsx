d5e5e4131d13ef2c5c95d242652e8cebd38445d4
HRSPATEL <mr.hrpatel@gmail.com>
2024-03-30 21:11:21 -0400

    contact works now

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_vugolpf',
+        'template_98z5u4r',
         {
           from_name: form.name,
           to_name: "Harsh",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "harsh.patel.states@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'FdZxBug7UrhoBxWkZ'
       )
       .then(
         () => {
